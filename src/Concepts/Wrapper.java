0f8d84df58434d030d027d6d11a78a3b16e2466f
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-04 23:01:36 +0530

    Wrapper

---
+++ src/Concepts/Wrapper.java
@@ -0,0 +1,15 @@
+package Concepts;
+
+public class Wrapper {
+
+
+
+    public static void main(String[] args) {
+        Integer a =10;
+        Character c ;
+        String str = "Hi";
+
+        System.out.println(a +" " + str );
+
+    }
+}
