78de27d8379939867abf5a1662a9a982e730e35a
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-06 21:17:49 +0530

    My Learnings

---
+++ src/Others/Basic.java
@@ -0,0 +1,21 @@
+package Others;
+
+public class Basic {
+
+    public static void sum(int x, int y)
+    {
+        int z= x+y;
+        System.out.println(z);
+    }
+
+
+    public static void main(String args[])
+{
+     sum(3,4);
+  //  System.out.println(p);
+
+
+
+}
+
+}
