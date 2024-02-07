515ad7b26779a1338d2dd777f44a6745138e50fd
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-07 21:05:09 +0530

    My Learnings

---
+++ src/Collections/arraylist.java
@@ -0,0 +1,21 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class arraylist {
+
+    public static void main(String[] args) {
+
+      ArrayList Items_1 = new ArrayList();
+
+       // ArrayList<String> Items_1 = new ArrayList<String>();  // This should have only Strings
+
+        Items_1.add(123);
+        Items_1.add("How are You");
+        Items_1.add("Bye");
+
+        System.out.println(Items_1);
+
+
+    }
+}
