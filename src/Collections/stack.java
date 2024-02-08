144f45bf616bcdf6c7fbd2afc10e07e287093f57
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-08 18:38:16 +0530

    stack

---
+++ src/Collections/stack.java
@@ -0,0 +1,36 @@
+package Collections;
+
+
+import java.util.Stack;
+
+public class stack {
+
+    public static void main(String[] args) {
+
+    Stack list = new Stack();
+        list.push("A");
+        list.push("B");
+        list.push("Test");
+        list.push("D");
+        System.out.println(list);
+        System.out.println(list.peek()); // This will show the last one
+        System.out.println(list.pop()); // This will remove the last one
+        System.out.println(list);
+        System.out.println(list.peek());
+        System.out.println(list);
+        System.out.println(list.pop());
+        System.out.println(list.pop());
+        System.out.println(list.peek());
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+    }
+
+}
