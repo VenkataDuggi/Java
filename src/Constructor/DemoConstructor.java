f7ffd9063b907eb6a46ead00bc99efc650ec94fe
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-15 19:41:20 +0530

    Constructor1

---
+++ src/Constructor/DemoConstructor.java
@@ -0,0 +1,18 @@
+package Constructor;
+
+public class DemoConstructor {
+
+  DemoConstructor()
+    {
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String[] args) {
+
+
+        DemoConstructor demo = new DemoConstructor();
+
+    }
+
+
+}
