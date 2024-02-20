658707c22958fb9e868c2a1a93c45be1d75b13a8
VenkataDuggi <narasimhavenkat555@gmail.com>
2024-02-20 14:21:42 +0530

    DC

--- src/Constructor/DemoConstructor.java
+++ src/Constructor/DemoConstructor.java
@@ -4,13 +4,19 @@ public class DemoConstructor {
 
   DemoConstructor()
     {
-        System.out.println("Constructor called");
+        System.out.println("Default Constructor called");
     }
 
     public static void main(String[] args) {
 
 
         DemoConstructor demo = new DemoConstructor();
+        new DemoConstructor();  //We cac call constructor like this also
+
+        DemoConstructor DC = null;
+
+        DC= new DemoConstructor();
+
 
     }
 
